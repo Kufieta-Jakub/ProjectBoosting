e5c3fd4b42c2f362bf6ece61f419dd66512017df
Kufieta-Jakub <kufietajakub@gmail.com>
2025-04-10 12:25:40 +0200

    pierwszy commit

---
+++ src/main/java/com/example/projectboosting/ProjectboostingApplication.java
@@ -0,0 +1,13 @@
+package com.example.projectboosting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectboostingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectboostingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/projectboosting/ProjectboostingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projectboosting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectboostingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
